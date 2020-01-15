20014e68de957bb34d2dc2d1ac742e3613a27b29
dinhmai74 <anh.dinh74@gmail.com>
2020-01-15 09:42:50 +0700

    edit gluegon

--- gluegun/lo/src/commands/screen.ts
+++ gluegun/lo/src/commands/screen.ts
@@ -3,7 +3,7 @@ import { GluegunToolbox } from 'gluegun'
 enum Patterns {
   NAV_IMPORTS_SCREENS = '} from "../screens',
   NAV_IMPORTS_NAVIGATORS = 'import[\\s\\S]*from\\s+"react-navigation";?',
-  ROOT_NAV_ROUTES = 'export const RootNavigator.+[\\s\\S]\\s+{',
+  ROOT_NAV_ROUTES = 'export const PrimaryNavigator.+[\\s\\S]\\s+{',
   NAV_ROUTES = 'export const [a-zA-Z0-9]+ = create[a-zA-Z]+[(][{]'
 }
 
